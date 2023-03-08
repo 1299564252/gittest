32cdfee4f29533415e339fdfd698fbd3041bc49a
tfyt <1299564252@qq.com>
2023-03-08 18:02:39 +0800

    first commit

---
+++ src/main/java/com/atguigu/git.java
@@ -0,0 +1,9 @@
+package com.atguigu;
+
+public class git {
+    public static void main(String[] args) {
+        System.out.println("Hello git1");
+        System.out.println("Hello git2");
+        System.out.println("Hello git3");
+    }
+}
